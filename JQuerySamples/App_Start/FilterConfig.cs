2c4c376128c29fd88549a94d20313cc38c0f000a
symba2003 <symba2003@hotmail.com>
2014-02-14 13:44:32 +0400

    Added Sample projects for JQuery learning

---
+++ JQuerySamples/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace JQuerySamples
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
